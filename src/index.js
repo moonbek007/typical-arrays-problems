5cacec4c56e88e57d44e5f4e7f367f85d192f07b
moonbek <oybek4ik.mamatov.00@mail.ru>
2021-03-15 10:41:24 +0500

    feat: solve the typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    let answer = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < answer) {
+            answer = array[i];
+        }
+    }
+    return answer;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    let answer = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > answer) {
+            answer = array[i];
+        }
+    }
+    return answer;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
+};
